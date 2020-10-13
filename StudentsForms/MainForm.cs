f2bc006ba7ccbbede3c7b86aee0af82be6061cf4
Infarh <shmachilin@yandex.ru>
2020-10-13 10:23:38 +0300

    Настройка визуальной части

--- StudentsForms/MainForm.cs
+++ StudentsForms/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace StudentsForms
@@ -8,6 +9,38 @@ namespace StudentsForms
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void OpenFileButton_Click(object sender, EventArgs e)
+        {
+            //var open_dialog = new OpenFileDialog
+            //{
+            //    Title = "Выбор файла данных",
+            //    Filter = "csv-файлы (*.csv)|*.csv|Все файлы (*.*)|*.*"
+            //};
+
+            //if(open_dialog.ShowDialog() != DialogResult.OK) return;
+
+            //MessageBox.Show(open_dialog.FileName);
+
+            if (OpenStudentsDataFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            var file_name = OpenStudentsDataFileDialog.FileName;
+
+            LoadStudentsFromFile(file_name);
+        }
+
+        private static void LoadStudentsFromFile(string FileName)
+        {
+            using (var reader = new StreamReader(FileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    var components = line.Split(';');
+                }
+            }
         }
     }
 }
