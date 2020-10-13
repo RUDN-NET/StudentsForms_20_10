cdbf2f16ed6187b99fc6ce74fcbc8284690140e9
Infarh <shmachilin@yandex.ru>
2020-10-13 10:02:15 +0300

    Удаление хлама

--- StudentsForms/Form1.cs
+++ StudentsForms/Form1.cs
@@ -9,27 +9,5 @@ namespace StudentsForms
         {
             InitializeComponent();
         }
-
-        private void HelloButton_Click(object sender, EventArgs e)
-        {
-            HelloButton.Text = "123";
-
-            MessageBox.Show("Hello World!", "Привет!", MessageBoxButtons.OK, MessageBoxIcon.Question);
-        }
-
-        private void HelloButton_OnMouseMove(object sender, MouseEventArgs e)
-        {
-            HelloButton.Text = string.Format("{0}:{1}", e.Location.X, e.Location.Y);
-        }
-
-        private void TimerControlCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-            InformationTimer.Enabled = TimerControlCheckBox.Checked;
-        }
-
-        private void InformationTimer_Tick(object sender, EventArgs e)
-        {
-            InformationLabel.Text = DateTime.Now.ToString("HH:mm:ss.ffff");
-        }
     }
 }
